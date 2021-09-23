c8d16ea1386cc63d6275d93f16759fa2df33523d
Teyhan USLU <teyhanytu@gmail.com>
2021-09-23 09:15:10 +0300

    Initial Commit

---
+++ BusStation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusStation
+//
+//  Created by teyhan.uslu on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
